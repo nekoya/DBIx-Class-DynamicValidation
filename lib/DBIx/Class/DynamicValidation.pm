cae14ece80c2919c453266e507724af7215ae5cd
Ryo Miyake <ryo.studiom@gmail.com>
2009-07-14 16:39:30 +0900

    Added comments

--- lib/DBIx/Class/DynamicValidation.pm
+++ lib/DBIx/Class/DynamicValidation.pm
@@ -15,7 +15,40 @@ DBIx::Class::DynamicValidation
 
 =head1 SYNOPSIS
 
+Write 'validation' method in your schema object class.
+
+    sub validation {
+        my $self = shift;
+        [
+            name      => [ qw/NOT_BLANK/ ],
+            email     => [ qw/EMAIL_LOOSE/ ],
+            introduce => [ qw/NOT_BLANK/, [ qw/LENGTH 0 50000/ ] ],
+        ];
+    }
+
+If you want to use any plugins for FV::Simple, set 'validator_plugins'.
+
+    sub validation {
+        my $self = shift;
+        [
+            name      => [ qw/NOT_BLANK ASCII/, [ qw/LENGTH 0 255/ ] ],
+            email     => [ qw/NOT_BLANK/, [ qw/LENGTH 0 255/ ] ],
+            password  => [ qw/NOT_BLANK/, [ qw/LENGTH 0 255/ ] ],
+            deleted   => [ qw/UINT/, [ qw/BETWEEN 0 1/ ] ],
+
+            { name   => [ qw/id name/ ] } => [ [ 'DBIC_UNIQUE', $self->result_source->resultset, '!id', 'name' ] ],
+        ];
+    }
+
+    __PACKAGE__->validator_plugins( qw(
+        FormValidator::Simple::Plugin::DBIC::Unique
+    ) );
+
 =head1 DESCRIPTION
+
+Validate model object when they have inserted or updated automatically.
+
+You can use FormValidator::Simple and their plugins as a validator.
 
 =head1 METHODS
 
